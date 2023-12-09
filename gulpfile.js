659142778c319947b3e29ec25943c13be9a1e877
Lily <dobbyssockk@outlook.com>
2023-12-10 00:07:18 +0400

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -7,75 +7,96 @@ import autoprefixer from 'gulp-autoprefixer';
 import cleanCSS from 'gulp-clean-css';
 import imagemin from 'gulp-imagemin';
 import htmlmin from 'gulp-htmlmin';
-import replace from 'gulp-replace';
 
 const sass = sassModule(nodeSass);
 
-// Static server
-gulp.task('server', function() {
-
+gulp.task("server", function () {
     browserSync({
         server: {
-            baseDir: "dist"
-        }
+            baseDir: "dist",
+        },
     });
 
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch("src/*.html").on("change", browserSync.reload);
 });
 
-gulp.task('styles', function() {
-    return gulp.src("src/sass/**/*.+(scss|sass)")
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename({suffix: '.min', prefix: ''}))
+gulp.task("styles", function () {
+    return gulp
+        .src("src/sass/**/*.+(scss|sass)")
+        .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+        .pipe(rename({ suffix: ".min", prefix: "" }))
         .pipe(autoprefixer({
             cascade: false
         }))
-        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(cleanCSS({ compatibility: "ie8" }))
         .pipe(gulp.dest("dist/css"))
         .pipe(browserSync.stream());
 });
 
-gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
-    gulp.watch("src/*.html").on('change', gulp.parallel('html'));
-})
+gulp.task("watch", function () {
+    gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel("styles"));
+    gulp.watch("src/*.html").on("change", gulp.parallel("html"));
+    gulp.watch("src/js/**/*.js").on("change", gulp.parallel("scripts"));
+    gulp.watch("src/fonts/**/*").on("all", gulp.parallel("fonts"));
+    gulp.watch("src/icons/**/*").on("all", gulp.parallel("icons"));
+    gulp.watch("src/img/**/*").on("all", gulp.parallel("images"));
+    gulp.watch("src/translations/**/*.json").on("change", gulp.parallel("translations"));
+});
 
-gulp.task('html', function() {
-    return gulp.src("src/*.html")
-        .pipe(htmlmin({collapseWhitespace: true}))
-        .pipe(replace('src="src/img/', 'src="img/'))
+gulp.task("html", function () {
+    return gulp
+        .src("src/*.html")
+        .pipe(htmlmin({ collapseWhitespace: true }))
         .pipe(gulp.dest("dist/"));
 });
 
-gulp.task('scripts', function() {
-    return gulp.src("src/js/**/*.js")
-        .pipe(gulp.dest("dist/js"));
+gulp.task("scripts", function () {
+    return gulp
+        .src("src/js/**/*.js")
+        .pipe(gulp.dest("dist/js"))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('fonts', function() {
-    return gulp.src("src/fonts/**/*")
-        .pipe(gulp.dest("dist/fonts"));
+gulp.task("fonts", function () {
+    return gulp
+        .src("src/fonts/**/*")
+        .pipe(gulp.dest("dist/fonts"))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('icons', function() {
-    return gulp.src("src/icons/**/*")
-        .pipe(gulp.dest("dist/icons"));
+gulp.task("icons", function () {
+    return gulp
+        .src("src/icons/**/*")
+        .pipe(gulp.dest("dist/icons"))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('mailer', function() {
-    return gulp.src("src/mailer/**/*")
-        .pipe(gulp.dest("dist/mailer"));
-});
-
-gulp.task('images', function() {
-    return gulp.src("src/img/**/*")
+gulp.task("images", function () {
+    return gulp
+        .src("src/img/**/*")
         .pipe(imagemin())
-        .pipe(gulp.dest("dist/img"));
+        .pipe(gulp.dest("dist/img"))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('translations', function() {
-    return gulp.src('translations/**/*.json')
+gulp.task("translations", function() {
+    return gulp
+        .src('src/translations/**/*.json')
         .pipe(gulp.dest('dist/translations'));
 });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'html', 'scripts', 'fonts', 'icons', 'mailer', 'images', 'translations'));
+
+gulp.task(
+    "default",
+    gulp.parallel(
+        "watch",
+        "server",
+        "styles",
+        "scripts",
+        "fonts",
+        "icons",
+        "html",
+        "images",
+        "translations"
+    )
+);
