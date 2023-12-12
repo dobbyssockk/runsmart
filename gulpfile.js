40b2106329923cadbc4ab65413e45f78c2144bd1
Lily <dobbyssockk@outlook.com>
2023-12-12 22:04:21 +0400

    Fix gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -10,106 +10,93 @@ import htmlmin from 'gulp-htmlmin';
 
 const sass = sassModule(nodeSass);
 
-gulp.task("server", function () {
+// Static server
+gulp.task('server', function () {
     browserSync({
         server: {
-            baseDir: "dist",
+            baseDir: 'dist',
         },
     });
 
-    gulp.watch("src/*.html").on("change", browserSync.reload);
+    gulp.watch('src/*.html').on('change', browserSync.reload);
 });
 
-gulp.task("styles", function () {
+gulp.task('styles', function () {
     return gulp
-        .src("src/sass/**/*.+(scss|sass)")
-        .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
-        .pipe(rename({ suffix: ".min", prefix: "" }))
+        .src('src/sass/**/*.+(scss|sass)')
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(rename({ suffix: '.min', prefix: '' }))
         .pipe(autoprefixer({
             cascade: false
         }))
-        .pipe(cleanCSS({ compatibility: "ie8" }))
-        .pipe(gulp.dest("dist/css"))
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(gulp.dest('dist/css'))
         .pipe(browserSync.stream());
 });
 
-gulp.task("watch", function () {
-    gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel("styles"));
-    gulp.watch("src/*.html").on("change", gulp.parallel("html"));
-    gulp.watch("src/js/**/*.js").on("change", gulp.parallel("scripts"));
-    gulp.watch("src/fonts/**/*").on("all", gulp.parallel("fonts"));
-    gulp.watch("src/icons/**/*").on("all", gulp.parallel("icons"));
-    gulp.watch("src/img/**/*").on("all", gulp.parallel("images"));
-<<<<<<< HEAD
-    gulp.watch("src/translations/**/*.json").on("change", gulp.parallel("translations"));
-=======
->>>>>>> 8c502bd7f837e16359e196b6fc7de0101f3d1fef
+gulp.task('watch', function () {
+    gulp.watch('src/sass/**/*.+(scss|sass|css)', gulp.parallel('styles'));
+    gulp.watch('src/*.html').on('change', gulp.parallel('html'));
+    gulp.watch('src/js/**/*.js').on('change', gulp.parallel('scripts'));
+    gulp.watch('src/fonts/**/*').on('all', gulp.parallel('fonts'));
+    gulp.watch('src/icons/**/*').on('all', gulp.parallel('icons'));
+    gulp.watch('src/img/**/*').on('all', gulp.parallel('images'));
+    gulp.watch('src/translations/**/*.json').on('change', gulp.parallel('translations'));
 });
 
-gulp.task("html", function () {
+gulp.task('html', function () {
     return gulp
-        .src("src/*.html")
+        .src('src/*.html')
         .pipe(htmlmin({ collapseWhitespace: true }))
-        .pipe(gulp.dest("dist/"));
+        .pipe(gulp.dest('dist/'));
 });
 
-gulp.task("scripts", function () {
+gulp.task('scripts', function () {
     return gulp
-        .src("src/js/**/*.js")
-        .pipe(gulp.dest("dist/js"))
+        .src('src/js/**/*.js')
+        .pipe(gulp.dest('dist/js'))
         .pipe(browserSync.stream());
 });
 
-gulp.task("fonts", function () {
+gulp.task('fonts', function () {
     return gulp
-        .src("src/fonts/**/*")
-        .pipe(gulp.dest("dist/fonts"))
+        .src('src/fonts/**/*')
+        .pipe(gulp.dest('dist/fonts'))
         .pipe(browserSync.stream());
 });
 
-gulp.task("icons", function () {
+gulp.task('icons', function () {
     return gulp
-        .src("src/icons/**/*")
-        .pipe(gulp.dest("dist/icons"))
+        .src('src/icons/**/*')
+        .pipe(gulp.dest('dist/icons'))
         .pipe(browserSync.stream());
 });
 
-gulp.task("images", function () {
+gulp.task('images', function () {
     return gulp
-        .src("src/img/**/*")
-<<<<<<< HEAD
+        .src('src/img/**/*')
         .pipe(imagemin())
-=======
->>>>>>> 8c502bd7f837e16359e196b6fc7de0101f3d1fef
-        .pipe(gulp.dest("dist/img"))
+        .pipe(gulp.dest('dist/img'))
         .pipe(browserSync.stream());
 });
 
-gulp.task("translations", function() {
+gulp.task('translations', function() {
     return gulp
-<<<<<<< HEAD
         .src('src/translations/**/*.json')
         .pipe(gulp.dest('dist/translations'));
 });
 
-
-=======
-        .src("src/translations/**/*.json")
-        .pipe(gulp.dest('dist/translations'));
-});
-
->>>>>>> 8c502bd7f837e16359e196b6fc7de0101f3d1fef
 gulp.task(
-    "default",
+    'default',
     gulp.parallel(
-        "watch",
-        "server",
-        "styles",
-        "scripts",
-        "fonts",
-        "icons",
-        "html",
-        "images",
-        "translations"
+        'watch',
+        'server',
+        'styles',
+        'scripts',
+        'fonts',
+        'icons',
+        'html',
+        'images',
+        'translations'
     )
 );
